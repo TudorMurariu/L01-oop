a471842d397ec3f6526669b6d2d199c75fb2bedb
Tudor <30391543+TudorMurariu@users.noreply.github.com>
2022-03-02 00:59:04 +0200

    done

--- 11.c
+++ 11.c
@@ -26,6 +26,9 @@ void problema(float x1,float y1,float x2,float y2,float x3,float y3,float x4,flo
     s2.b = x4 - x3;
     s2.c = x3*y4 - x4*y3;
 
+    //printf("%f %f %f\n",s1.a,s1.b,s1.c);
+    //printf("%f %f %f\n",s2.a,s2.b,s2.c);
+
     /// inmultim sistemul1 cu s2.a si sis2 cu s1.a
     ///apoi le scadem pentru a afla x
 
@@ -34,7 +37,7 @@ void problema(float x1,float y1,float x2,float y2,float x3,float y3,float x4,flo
 
     /// Avem punctul de intersectie a dreptelor
 
-    printf("%d %d",x,y);
+    printf("%f %f",x,y);
 }
 
 void p11()
@@ -42,12 +45,18 @@ void p11()
     /// citim punctele
     float x1,x2,x3,x4,y1,y2,y3,y4;
     printf("A = ");
-    scanf("%f %f",&x1,&y1);
+    scanf("%f",&x1);
+    scanf("%f",&y1);
     printf("B = ");
-    scanf("%f %f",&x2,&y2);
+    scanf("%f",&x2);
+    scanf("%f",&y2);
     printf("c = ");
-    scanf("%f %f",&x3,&y3);
+    scanf("%f",&x3);
+    scanf("%f",&y3);
     printf("D = ");
-    scanf("%f %f",&x4,&y4);
+    scanf("%f",&x4);
+    scanf("%f",&y4);
 
+    //printf("%f %f %f %f %f %f %f %f\n",x1,y1,x2,y2,x3,y3,x4,y4);
+    problema(x1,y1,x2,y2,x3,y3,x4,y4);
 }
